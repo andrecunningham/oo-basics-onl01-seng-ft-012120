9dd45b5bb7188d690bccf33e9c5e1efc59e6bc45
Andre Cunningham <andrecunningham62@gmail.com>
2020-01-31 01:03:43 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,8 @@
-# Make your shoe class here!
+describe 'Shoe' do
+  let(:shoe) { Shoe.new("Nike") }
+
+  describe '::new' do
+    it 'gets initialized with a brand' do
+      expect{Shoe.new("Adidas")}.to_not raise_error
+    end
+  end
